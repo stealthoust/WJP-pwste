a961dda571baffef35795a4e229fc713625b35f5
Shirobachi <simon@hryszko.dev>
2021-03-06 16:24:10 +0100

    Formatting changing

--- LABO1/src/LABO1/LABO1.java
+++ LABO1/src/LABO1/LABO1.java
@@ -1,57 +1,62 @@
 package LABO1;
 
 import java.io.*;
+
 public class LABO1 {
+
     //Funkcja obliczająca rekurencje
-    private static long factorialR(int n){
-        if(n==0) 
+    private static long factorialR(int n) {
+        if (n == 0) {
             return 1;
-        else 
-            return n*factorialR(n-1);
+        } else {
+            return n * factorialR(n - 1);
+        }
     }
-    
-    private static long factorialI(int n){
-        
+
+    private static long factorialI(int n) {
+
         int result = 1;
-        for(int i=n; i>0; i--)
+        for (int i = n; i > 0; i--) {
             result *= i;
+        }
         return result;
     }
-    
-    private static long powR(int a, int n){
-        if (n==0)
+
+    private static long powR(int a, int n) {
+        if (n == 0) {
             return 1;
-        else
-            return powR(a, n-1)*a;
+        } else {
+            return powR(a, n - 1) * a;
+        }
     }
-    
-    private static long powI(int a, int n){
-        
+
+    private static long powI(int a, int n) {
+
         long result = 1;
-        
-        if (n > 0)
-            for(int i = n; i>0; i--)
+
+        if (n > 0) {
+            for (int i = n; i > 0; i--) {
                 result *= a;
-        else
+            }
+        } else {
             a = -1;
+        }
         return result;
-        
+
     }
-    
-    private static int validInt(String text){
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+
+    private static int validInt(String text) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         boolean goodData = false;
         int data = -1;
-        
-        while(!goodData){
+
+        while (!goodData) {
             try {
                 System.out.print(text);
-                
+
                 data = Integer.parseInt(in.readLine());
                 goodData = true;
-            }
-            
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.err.println("Error: this is not int! Try again!");
                 goodData = false;
             }
@@ -60,39 +65,40 @@ public class LABO1 {
     }
 
     //zero = true mean that zero is acceptabled
-    public static int validPositiveInt(String text, boolean zeroAcceptabled){
-        
+    public static int validPositiveInt(String text, boolean zeroAcceptabled) {
+
         boolean goodData = false;
         int result = -1;
-        
-        while(!goodData){
+
+        while (!goodData) {
             result = validInt(text);
-            
-            if (!zeroAcceptabled && result <= 0){
+
+            if (!zeroAcceptabled && result <= 0) {
                 System.err.println("Error: this is not value greater than zero");
                 goodData = false;
-            }
-            else if (zeroAcceptabled && result < 0){
+            } else if (zeroAcceptabled && result < 0) {
                 System.err.println("Error: this is not value greater or equel zero");
                 goodData = false;
-            }
-            else
+            } else {
                 goodData = true;
+            }
         }
-        
+
         return result;
     }
 
     public static void main(String[] args) throws IOException {
-        
-        
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.readLine();
+
         //Factorial
         System.out.println("Let's calc factorial!");
-        
+
         int n = validPositiveInt("n = ", true);
-        System.out.println("R: "+n+"!="+factorialR(n));
-        System.out.println("I: "+n+"!="+factorialI(n));
-        
+        System.out.println("R: " + n + "!=" + factorialR(n));
+        System.out.println("I: " + n + "!=" + factorialI(n));
+
         //Power
         System.out.println("Let's make some power!");
         int a = validInt("a = ");
--- LABO2.2/src/labo2/pkg2/LABO22.java
+++ LABO2.2/src/labo2/pkg2/LABO22.java
@@ -1,21 +1,21 @@
 package labo2.pkg2;
+
 import java.io.*;
 
 public class LABO22 {
-    private static int validInt(String text){
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+
+    private static int validInt(String text) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         boolean goodData = false;
         int data = -1;
-        
-        while(!goodData){
+
+        while (!goodData) {
             try {
                 System.out.print(text);
-                
+
                 data = Integer.parseInt(in.readLine());
                 goodData = true;
-            }
-            
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.err.println("Error: this is not int! Try again!");
                 goodData = false;
             }
@@ -24,59 +24,63 @@ public class LABO22 {
     }
 
     //zero = true mean that zero is acceptabled
-    public static int validPositiveInt(String text, boolean zeroAcceptabled){
-        
+    public static int validPositiveInt(String text, boolean zeroAcceptabled) {
+
         boolean goodData = false;
         int result = -1;
-        
-        while(!goodData){
+
+        while (!goodData) {
             result = validInt(text);
-            
-            if (!zeroAcceptabled && result <= 0){
+
+            if (!zeroAcceptabled && result <= 0) {
                 System.err.println("Error: this is not value greater than zero");
                 goodData = false;
-            }
-            else if (zeroAcceptabled && result < 0){
+            } else if (zeroAcceptabled && result < 0) {
                 System.err.println("Error: this is not value greater or equel zero");
                 goodData = false;
-            }
-            else
+            } else {
                 goodData = true;
+            }
         }
-        
+
         return result;
     }
-    
-    private static int [][] transponeArray2(int [][] arr){
-        int [][] array = new int[arr[0].length][arr.length];
-        for(int i=0; i<arr[0].length; i++)
-            for(int s=0; s<arr.length; s++)
+
+    private static int[][] transponeArray2(int[][] arr) {
+        int[][] array = new int[arr[0].length][arr.length];
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int s = 0; s < arr.length; s++) {
                 array[i][s] = arr[s][i];
+            }
+        }
         return array;
     }
-    
-    private static int[][] makeArray2(int n, int m){
+
+    private static int[][] makeArray2(int n, int m) {
         int[][] result = new int[n][m];
-    
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                result[i][j] = validInt("arr["+i+"]["+j+"] = ");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = validInt("arr[" + i + "][" + j + "] = ");
+            }
+        }
         return result;
     }
-    
-    private static void showShortArray2(int [][] arr){
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++)
+
+    private static void showShortArray2(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + "\t");
+            }
             System.out.println();
         }
     }
-    
+
     public static void main(String[] args) {
         int r = validPositiveInt("How many rows? ", false);
         int c = validPositiveInt("How many columns? ", false);
-        
-        int [][]array = makeArray2(r, c);
+
+        int[][] array = makeArray2(r, c);
         showShortArray2(array);
         showShortArray2(transponeArray2(array));
     }
