fde02746bd6091895c5ad7a54009bf0b4ada9925
Shirobachi <simon@hryszko.dev>
2021-03-17 16:42:50 +0100

    TBU

---
+++ LABO/5/src/Window.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Window extends Frame {
+  public static void main(String[] args) throws Exception {
+    
+    Window a = new Window();
+    a.setBounds(10, 10, 200, 200);
+    a.setVisible(true);
+    
+
+
+  }
+}
