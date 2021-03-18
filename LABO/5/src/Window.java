3ce5b9e3586c6ffd9ec9f3e4ac3276d99e1398b5
Shirobachi <simon@hryszko.dev>
2021-03-18 14:36:24 +0100

    The very first window app

--- LABO/5/src/Window.java
+++ LABO/5/src/Window.java
@@ -2,12 +2,9 @@ import java.awt.*;
 
 public class Window extends Frame {
   public static void main(String[] args) throws Exception {
-    
+
     Window a = new Window();
     a.setBounds(10, 10, 200, 200);
     a.setVisible(true);
-    
-
-
   }
 }
