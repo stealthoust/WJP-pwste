67789bdbb482b0b75439e7dfd626390046286099
Shirobachi <simon@hryszko.dev>
2021-03-21 04:38:40 +0100

    Application with windows
    - For now needs to be fixed

--- LABO/5.2/src/Window.java
+++ LABO/5.2/src/Window.java
@@ -1,62 +1,64 @@
 import java.awt.Frame;
-import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class Window extends Frame implements WindowListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Button;
+
+public class Window extends Frame implements WindowListener, ActionListener {
+
     public Window() throws HeadlessException {
         super();
-        setBounds(10, 10, 400, 400);
+        setBounds(1250, 250, 500, 250);
         setVisible(true);
         this.addWindowListener(this);
+
+        Button rainbowButton = new Button("Hello world!");
+        rainbowButton.setBounds(100, 50, 200, 25);
+        rainbowButton.setVisible(true);
+        add(rainbowButton);
+
+        this.setLayout(null);
     }
 
     @Override
     public void windowOpened(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void windowClosing(WindowEvent e) {
-        // TODO Auto-generated method stub
         System.exit(0);
     }
 
     @Override
     public void windowClosed(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void windowIconified(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void windowDeiconified(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void windowActivated(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void windowDeactivated(WindowEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
-    public static void main(String[] args) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+        setTitle("Przycisk wciśnięty");
+    }
+
+    public static void main(String[] args) throws Exception {
         Window win = new Window();
-
     }
-
 }
